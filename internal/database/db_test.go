1787d5879c5b223a58ceace6de4c360422dc1e0c
ivanglie <ivanglie22@gmail.com>
2023-06-15 23:18:25 +0300

    Add one more test

--- internal/database/db_test.go
+++ internal/database/db_test.go
@@ -56,6 +56,22 @@ func (m *MockClientError) Do(req *http.Request) (*http.Response, error) {
 	}, errors.New("something went wrong")
 }
 
+func TestDB_Search(t *testing.T) {
+	db := &DB{chunks: []string{"../../test/data/DB_0001.CSV", "../../test/data/DB_0002.CSV", "../../test/data/DB_0003.CSV"}}
+	loc, err := db.Search("8.8.8.8")
+	assert.Nil(t, err)
+	assert.NotNil(t, loc)
+	assert.NotNil(t, loc.Properties)
+	assert.Equal(t, "US", loc.Properties[Code])
+	assert.Equal(t, "United States of America", loc.Properties[Country])
+	assert.Equal(t, "California", loc.Properties[Region])
+	assert.Equal(t, "Mountain View", loc.Properties[City])
+	assert.Equal(t, "37.405992", loc.Properties[Latitude])
+	assert.Equal(t, "-122.078515", loc.Properties[Longitude])
+	assert.Equal(t, "94043", loc.Properties[ZipCode])
+	assert.Equal(t, "-07:00", loc.Properties[TimeZone])
+}
+
 func TestDB_Split(t *testing.T) {
 	f, _ := os.Open("../../test/data/DB.zip")
 	info, _ := f.Stat()
